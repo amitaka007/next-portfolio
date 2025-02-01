0004f15772ec9b66f9605ac6060d2a23761651ca
xeamdesigner6 <xeam.designer6@gmail.com>
2025-02-01 18:23:31 +0530

    final changes

---
+++ app/page.tsx
@@ -0,0 +1,52 @@
+import { FaNodeJs } from "react-icons/fa"
+import HomePage from "./HomePage"
+// import { ref, get } from "firebase/database"
+// import { database } from "@/firebase"
+
+// async function getData() {
+//   // return await (await get(ref(database))).val()
+//   const DB_URL = process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL + '/.json'
+//   const res = await fetch(DB_URL, { cache: 'no-store' })
+
+//   const data = res.json()
+//   return data
+// }
+
+async function getData() {
+  // Check if we are in a development environment
+  if (process.env.NODE_ENV === 'development') {
+    // If in development, use local mock data
+    const data = require('../data.json');
+    return data;
+  }
+
+  // In production or other environments, fetch data from Firebase
+  const DB_URL = process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL + '/.json';
+  const res = await fetch(DB_URL, { cache: 'no-store' });
+
+  // Ensure the response is parsed correctly
+  const data = await res.json();
+  return data;
+}
+
+
+
+
+export default async function page() {
+
+  const data = await getData()
+  console.log(data,"Ddsas")
+
+  return (
+    <>
+      {data ?
+        <HomePage data={data} />
+        :
+        <div className='h-screen w-screen flex flex-col items-center justify-center gap-5 text-violet-600 fixed z-30 bg-gray-100 dark:bg-grey-900'>
+          <FaNodeJs size={100} className='animate-pulse' />
+          <p className='animate-pulse text-xl'>Loading...</p>
+        </div>
+      }
+    </>
+  )
+}
